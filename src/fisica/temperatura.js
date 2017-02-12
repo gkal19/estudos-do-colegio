7be65e91b0b8c3153a6258b3143a8253aa055b34
Gabriel Kalani <gabrielsilva1956@gmail.com>
2017-02-12 12:16:08 -0200

    FÍSICA - TEMPERATURA
    Adicionado um arquivo onde você pode fazer a conversão de Temperatura entre Celsius, Fahrenheit e Kelvin.

---
+++ src/fisica/temperatura.js
@@ -0,0 +1,17 @@
+// Celsius para Fahrenheit
+const CtoF = (celsius) => (celsius * 1.8  + 32)
+
+// Celsius para Kelvin
+const CtoK = (celsius) => (celsius + 273.15)
+
+// Fahrenheit para Celsius
+const FtoC = (fahrenheit) => (fahrenheit - 32 ) / 1.8
+
+// Fahrenheit para Kelvin
+const FtoC = (fahrenheit) => (fahrenheit * 1.8) - 459.67
+
+// Kelvin para Celsius
+const KtoC = (kelvin) => (kelvin - 273.15)
+
+// Kelvin para Fahrenheit
+const KtoF = (kelvin) => (kelvin * 1.8) - 459.67
