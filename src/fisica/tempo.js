c31d40fc03ad0809ad03a0b5f2a2c15f6c2f9d28
Jean Carlo Nascimento <jnascimento@gmail.com>
2017-02-14 22:11:44 -0200

    Update tempo.js
    NAO ABREVIE OS NOMES

--- src/fisica/tempo.js
+++ src/fisica/tempo.js
@@ -1,11 +1,11 @@
 // Segundos para Minutos
-const stomin = (s) => (s * 1 / 60)
+const secondsToMinutes = (s) => (s * 1 / 60)
 
 // Minuto para Hora
-const mintoh = (min) => (min * 1 / 60)
+const minutesToHours = (min) => (min * 1 / 60)
 
 // Minuto para Segundos
-const mintos = (min) => (s * 60)
+const minutesToSeconds = (min) => (s * 60)
 
 // Hora para Minuto
-const htomin = (h) => (h * 60)
+const HoursToMinutes = (h) => (h * 60)
